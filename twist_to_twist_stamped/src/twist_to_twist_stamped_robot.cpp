16490c8c24226e01aaae11f1be9ea34f2ba8df6f
Madhushan R.M.S <sadeepm20@gmail.com>
2024-06-28 22:11:43 +0800

    update twist_to_twist_stamped pkg

---
+++ twist_to_twist_stamped/src/twist_to_twist_stamped_robot.cpp
@@ -0,0 +1,37 @@
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
+
+class TwistToTwistStampedRobot : public rclcpp::Node
+{
+public:
+  TwistToTwistStampedRobot() : Node("twist_to_twist_stamped_robot")
+  {
+    twist_subscriber_ = this->create_subscription<geometry_msgs::msg::Twist>(
+      "/cmd_vel", 10,
+      std::bind(&TwistToTwistStampedRobot::twist_callback, this, std::placeholders::_1));
+    
+    twist_stamped_publisher_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("/diffbot_base_controller/cmd_vel", 10);
+  }
+
+private:
+  void twist_callback(const geometry_msgs::msg::Twist::SharedPtr msg)
+  {
+    auto twist_stamped_msg = geometry_msgs::msg::TwistStamped();
+    twist_stamped_msg.header.stamp = this->now();
+    twist_stamped_msg.twist = *msg;
+
+    twist_stamped_publisher_->publish(twist_stamped_msg);
+  }
+
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twist_subscriber_;
+  rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_stamped_publisher_;
+};
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<TwistToTwistStampedRobot>());
+  rclcpp::shutdown();
+  return 0;
+}
